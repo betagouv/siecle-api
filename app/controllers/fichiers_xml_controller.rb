629bd7bf80bb388397594cac192fbb3e50d116e6
Philippe Blayo <philippe.blayo@gmail.com>
2019-11-05 16:28:33 +0100

    Corrige l'invocation du background job
    Co-authored-by: yaf <yannick.francois@beta.gouv.fr>

--- app/controllers/fichiers_xml_controller.rb
+++ app/controllers/fichiers_xml_controller.rb
@@ -4,9 +4,9 @@ class FichiersXmlController < ApplicationController
   def create
     contenu = File.read(params[:fichier], :encoding => 'iso-8859-15')
     @fichier_xml = FichierXml.new(contenu: contenu)
-    ExtraitDonneesJob.perform_later(@fichier_xml.id)
 
     if @fichier_xml.save
+      ExtraitDonneesJob.perform_later(@fichier_xml.id)
       render json: @fichier_xml, status: :created
     else
       render json: @fichier_xml.errors, status: :unprocessable_entity
